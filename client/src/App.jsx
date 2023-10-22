7371a99e14cfa49cbd082ceba000ea3fd08829c0
Semih <shouldahjdh@gmail.com>
2023-10-22 12:38:48 +0300

    new files for routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignOut from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+
 function App() {
-  return (
-    <div className="">
-      <h1 className="" > Hello</h1>
-    </div>
-  );
+ return (
+  <BrowserRouter>
+ 
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/sign-in" element={<Signin/>}/>
+    <Route path="/sign-up" element={<SignUp/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/profile" element={<Profile/>}/>
+
+  </Routes>
+ 
+ </BrowserRouter>)
 }
 
 export default App;

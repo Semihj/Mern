98652cd3aac6c8e1dce6de87dc672eee2a987111
Semih <shouldahjdh@gmail.com>
2023-10-25 21:07:44 +0300

    complete sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -52,7 +52,7 @@ const navigate = useNavigate()
 
 
   }
-  console.log(formData)
+  
   return (
     <div className="p-3 max-w-lg mx-auto  ">
       <h1 className='text-3xl text-center font-semibold my-7 ' >Sign Up</h1>

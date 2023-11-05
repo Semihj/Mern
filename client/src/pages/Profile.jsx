e2e24fbd607164cd2e4dd319b2283752fefeddf2
Semih <shouldahjdh@gmail.com>
2023-11-05 05:15:29 +0300

    complete update listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -285,8 +285,10 @@ export default function Profile() {
 
             <div className="flex flex-col items-center">
               <button className='text-red-700 uppercase ' onClick={() => handleDeleteListing(listing._id)  } >Delete</button>
-              <button className='text-green-700 uppercase ' >Edit</button>
-            </div>
+              
+              <Link to={`/update-listing/${listing._id}`}>
+                 <button className='text-green-700 uppercase '  >Edit</button>
+            </Link></div>
       
           </div>
         ) ) }
